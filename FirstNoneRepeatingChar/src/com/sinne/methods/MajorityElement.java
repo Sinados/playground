c4d9d16bf73b28530149bb95254df712c86d5062
Sinados Yemane <sinnegy@gmail.com>
2017-10-04 00:36:54 -0500

    = most repeated Element in an array, provided that there is at least one number repeated more than half size of the array and the array is not empty

--- FirstNoneRepeatingChar/src/com/sinne/methods/MajorityElement.java
+++ FirstNoneRepeatingChar/src/com/sinne/methods/MajorityElement.java
@@ -2,4 +2,45 @@ package com.sinne.methods;
 
 public class MajorityElement {
 
+	int[] count = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+
+	int[] array = { 5, 3, 5, 3, 5, 1, 5, 3, 5, 6, 5, 4, 5 };
+	int k = 0;
+
+	int majorityElement = 0;
+	
+	public int majorityElement(int[] numbers) {
+		
+
+		for (int i = 0; i < numbers.length; i++) {
+			for (int j = i + 1; j < numbers.length; j++) {
+				if ((numbers[i] == numbers[j])) {
+
+					count[i] = count[i] + 1;
+
+					if (count[i] > (numbers.length / 2)) {
+						majorityElement = numbers[i];
+						
+					}
+
+				}
+
+			}
+
+		}
+
+		
+		return majorityElement;
+	}
+public static void main(String[] args) {
+		
+	
+		MajorityElement majorityE = new MajorityElement();
+		int[] abc ={5, 3, 5, 3, 5, 3, 2, 3, 5, 3, 4, 3, 3};
+		
+		System.out.println(majorityE.majorityElement(abc));
+
+		
+	}
+
 }
